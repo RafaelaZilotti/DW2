3cd3b85fa7ca723f8f2239f50979e1752c94de98
RafaelaZilotti <164359029+RafaelaZilotti@users.noreply.github.com>
2025-06-02 08:53:58 -0300

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,11 +1,15 @@
-import './App.css'
 import Header from './Header'
+import Footer from './Footer'
+import MainContent from './MainContent'
+import './App.css';
 
 function App() {
 
   return (
-    <div className='app'>
+    <div className="app">
       <Header />
+      <MainContent />
+      <Footer />
     </div>
   );
 
